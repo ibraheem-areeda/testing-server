7af59cf119ab782dc536b9bfb9d1c74560c6bb93
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-10-30 06:07:46 +0000

    done

--- things/urls.py
+++ things/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ThingList, ThingDetail
+from .views import ThingList, ThingDetail,charge_webhook
 
 urlpatterns = [
     path("", ThingList.as_view(), name="thing_list"),
     path("<int:pk>/", ThingDetail.as_view(), name="thing_detail"),
+    path("charge-webhook/", charge_webhook, name="thing_detail"),
 ]
--- things/views.py
+++ things/views.py
@@ -5,6 +5,9 @@ from rest_framework.generics import (
 from .models import Thing
 from .permissions import IsOwnerOrReadOnly
 from .serializers import ThingSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class ThingList(ListCreateAPIView):
@@ -16,3 +19,14 @@ class ThingDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
     queryset = Thing.objects.all()
     serializer_class = ThingSerializer
+
+
+@api_view(['GET', 'POST'])
+def charge_webhook(request):
+    if request.method == 'GET':
+        return Response("this is get method")
+    elif request.method == 'POST':
+        print("object well receved")
+        print(request.data)
+        return Response(request.data, status=status.HTTP_201_CREATED)
+
